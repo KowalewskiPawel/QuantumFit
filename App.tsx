2e1149cf89d900da6efe6eca0573108845ae5c12
KowalewskiPawel <53332984+KowalewskiPawel@users.noreply.github.com>
2023-12-16 16:43:33 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { PaperProvider } from "react-native-paper";
 import { Main } from "./src/Main";
 import { darkTheme, lightTheme } from "./src/themes";
@@ -10,9 +10,6 @@ export default function App() {
   const colorScheme = useColorScheme();
 
   const appTheme = colorScheme === "dark" ? darkTheme : lightTheme;
-  useEffect(() => {
-    console.log({colorScheme, appTheme})
-  }, [colorScheme, appTheme]);
   
   return (
     <PaperProvider theme={appTheme}>
