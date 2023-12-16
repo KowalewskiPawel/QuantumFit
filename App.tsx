5a291f1bbfd0bccd8379c1fda52cd651a4ba9a99
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-12-16 20:43:39 +0100

    chore: temporarily disable light mode

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { PaperProvider } from "react-native-paper";
 import { Main } from "./src/Main";
-import { CombinedLightTheme, CombinedDarkTheme } from "./src/themes";
+import { CombinedDarkTheme } from "./src/themes";
 import { Provider as StoreProvider } from "react-redux";
 import { store } from "./src/app/store";
-import { useColorScheme } from "react-native";
 
 export default function App() {
-  const colorScheme = useColorScheme();
-  const appTheme = colorScheme === "dark" ? CombinedDarkTheme : CombinedLightTheme;
+  // Disable Light/Dark Mode for now
+  // const colorScheme = useColorScheme();
+  // const appTheme = colorScheme === "dark" ? CombinedDarkTheme : CombinedLightTheme;
 
   return (
-    <PaperProvider theme={appTheme}>
+    <PaperProvider theme={CombinedDarkTheme}>
       <StoreProvider store={store}>
         <Main />
       </StoreProvider>
