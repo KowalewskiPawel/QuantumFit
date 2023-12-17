e1bdd26a78c0b98b75814ba2bf7c71519200fd7d
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-12-17 09:47:27 +0100

    fix: add scroll to lifestyle

--- src/screens/RegisterScreenLifestyle.tsx
+++ src/screens/RegisterScreenLifestyle.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, View, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView, ScrollView } from "react-native";
 import { Button, Dialog, Portal, useTheme } from "react-native-paper";
 import { styles } from "../styles/globalStyles";
 import { CustomCard, StackRow } from "../components";
@@ -53,64 +53,66 @@ export const RegisterScreenLifestyle = ({ navigation }) => {
 
   return (
     <SafeAreaView style={{ ...styles.container }}>
-      <View>
-        <View style={styles.textBackground}>
-          <Text style={{ ...styles.title, color: theme.colors.onBackground }}>
-            Your Lifestyle
-          </Text>
-        </View>
-        <View style={{ display: "flex", alignItems: "center" }}>
-          {LIFESTYLES.map((lifestyle) => (
-            <CustomCard
-              key={lifestyle.title}
-              title={lifestyle.title}
-              content={lifestyle.content}
-              icon={lifestyle.icon}
-              onPress={setSelectedLifestyle}
-              selected={selectedLifestyle === lifestyle.title}
-            />
-          ))}
-          <StackRow>
-            <Button
-              icon="arrow-left"
-              mode="contained"
-              onPress={() => navigation.goBack()}
-              style={{ marginTop: 20, marginBottom: 20, marginRight: 10 }}
-            >
-              Previous
-            </Button>
-            <Button
-              mode="contained"
-              style={{ marginTop: 20, marginBottom: 20 }}
-              onPress={validateRegistration}
-            >
-              Continue
-            </Button>
-          </StackRow>
-        </View>
-        <Portal>
-          <Dialog
-            visible={isError}
-            onDismiss={() => setIsError(false)}
-            style={{
-              backgroundColor: theme.colors.primaryContainer,
-            }}
-          >
-            <Dialog.Title>Please Select Lifestyle</Dialog.Title>
-            <Dialog.Content>
-              <Text style={{ color: "#FFF" }}>
-                Please select lifestyle that describes yours best, and then
-                click "continue".
-              </Text>
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button textColor="#FFF" onPress={() => setIsError(false)}>
-                OK
+      <ScrollView>
+        <View style={{ ...styles.container }}>
+          <View style={styles.textBackground}>
+            <Text style={{ ...styles.title, color: theme.colors.onBackground }}>
+              Your Lifestyle
+            </Text>
+          </View>
+          <View style={{ display: "flex", alignItems: "center" }}>
+            {LIFESTYLES.map((lifestyle) => (
+              <CustomCard
+                key={lifestyle.title}
+                title={lifestyle.title}
+                content={lifestyle.content}
+                icon={lifestyle.icon}
+                onPress={setSelectedLifestyle}
+                selected={selectedLifestyle === lifestyle.title}
+              />
+            ))}
+            <StackRow>
+              <Button
+                icon="arrow-left"
+                mode="contained"
+                onPress={() => navigation.goBack()}
+                style={{ marginTop: 20, marginBottom: 20, marginRight: 10 }}
+              >
+                Previous
               </Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
-      </View>
+              <Button
+                mode="contained"
+                style={{ marginTop: 20, marginBottom: 20 }}
+                onPress={validateRegistration}
+              >
+                Continue
+              </Button>
+            </StackRow>
+          </View>
+          <Portal>
+            <Dialog
+              visible={isError}
+              onDismiss={() => setIsError(false)}
+              style={{
+                backgroundColor: theme.colors.primaryContainer,
+              }}
+            >
+              <Dialog.Title>Please Select Lifestyle</Dialog.Title>
+              <Dialog.Content>
+                <Text style={{ color: "#FFF" }}>
+                  Please select lifestyle that describes yours best, and then
+                  click "continue".
+                </Text>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button textColor="#FFF" onPress={() => setIsError(false)}>
+                  OK
+                </Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
