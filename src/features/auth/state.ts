3f5d8e8ceab8b3f66c0352003d131bf23de1a724
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-12-17 17:56:39 +0100

    chore: update auth slice with uid

--- src/features/auth/state.ts
+++ src/features/auth/state.ts
@@ -3,6 +3,7 @@ import type { RootState } from "../../app/store";
 export type AuthState = {
     token: string | null;
     username: string | null;
+    uid: string | null;
     loginTime: number | null;
     loading: boolean;
     error: string | null;
