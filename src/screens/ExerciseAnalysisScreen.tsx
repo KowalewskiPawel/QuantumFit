93d984c44eb830d25c91a648a41657084d8bdd04
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-12-21 09:14:08 +0100

    feat: add Camera Recording

--- src/screens/ExerciseAnalysisScreen.tsx
+++ src/screens/ExerciseAnalysisScreen.tsx
@@ -1,28 +1,348 @@
 import React from "react";
-import { Text, View, SafeAreaView } from "react-native";
-import { Button, useTheme } from "react-native-paper";
+import {
+  Camera,
+  CameraType,
+} from "expo-camera";
+import { useEffect, useRef, useState } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import {
+  Button,
+  IconButton,
+  MD3Colors,
+  Text,
+  useTheme,
+} from "react-native-paper";
+import { Video } from "expo-av";
 import { styles } from "../styles/globalStyles";
+import { uploadToFirebase } from "../firebase/firebase-config";
+import { useAppSelector } from "../app/store";
+import { selectAuthState } from "../features/auth";
+
+type CameraCapturedVideo = {
+  uri: string;
+};
 
 export const ExerciseAnalysisScreen = ({ navigation }) => {
   const theme = useTheme();
+  const windowWidth = Dimensions.get("window").width;
+  const { uid } = useAppSelector(selectAuthState);
+  const cameraWidth = windowWidth * 0.9;
+  const cameraHeight = cameraWidth * (4 / 3);
+  const [countdown, setCountdown] = useState(5);
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordingCountdown, setRecordingCountdown] = useState(10);
+  const [showCountDown, setShowCountdown] = useState(false);
+  const countdownTimer = useRef(null);
+  const recordingCountdownTimer = useRef(null);
+  const [uploadStatus, setUploadStatus] = useState(null);
 
-  return (
-    <SafeAreaView style={{ ...styles.container }}>
-      <View>
-        <View style={styles.textBackground}>
-          <Text style={{ ...styles.title, color: theme.colors.onBackground }}>
-            Exercise Analysis
-          </Text>
-        </View>
-        <Button
-          icon="arrow-left"
-          mode="contained"
-          onPress={() => navigation.goBack()}
-          style={{ marginTop: 20, marginBottom: 20, marginRight: 10 }}
-        >
-          Go back
+  const [type, setType] = useState(CameraType.front);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef<Camera>(null);
+  const takePictureTimeout = useRef(null);
+  const [takenPicture, setTakenPicture] = useState<CameraCapturedVideo>();
+
+  const toggleCameraType = () => {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  };
+
+  const takePicture = () => {
+    setCountdown(5);
+    setShowCountdown(true);
+    takePictureTimeout.current = setTimeout(async () => {
+      setIsRecording(true);
+      setRecordingCountdown(10);
+      const video = await cameraRef.current.recordAsync({
+        quality: '480p',
+        maxDuration: 10,
+        maxFileSize: 9000000,
+        mute: true,
+      });
+      setTakenPicture(video);
+      setIsRecording(false);
+      setShowCountdown(false);
+    }, 5000);
+  };
+
+  const uploadVideoToFirebase = async () => {
+    setUploadStatus(null);
+    try {
+      const uploadResponse: any = await uploadToFirebase(
+        takenPicture.uri,
+        `${uid}_exercise_${Date.now().toFixed()}`,
+        false,
+        (currentUploadStatus) => {
+          setUploadStatus(Math.ceil(currentUploadStatus));
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setUploadStatus(null);
+    }
+  };
+
+  useEffect(() => {
+    if (countdown >= 5) {
+      clearInterval(countdownTimer.current);
+      countdownTimer.current = setInterval(
+        () => setCountdown((v) => v - 1),
+        1000
+      );
+    }
+    if (countdown === 0) {
+      clearInterval(countdownTimer.current);
+    }
+  }, [countdown]);
+
+  useEffect(() => {
+    if (recordingCountdown >= 10) {
+      clearInterval(recordingCountdownTimer.current);
+      recordingCountdownTimer.current = setInterval(
+        () => setRecordingCountdown((v) => v - 1),
+        1000
+      );
+    }
+    if (recordingCountdown === 0) {
+      clearInterval(recordingCountdownTimer.current);
+    }
+  }
+  , [recordingCountdown]);
+
+  useEffect(() => {
+    if (!!takenPicture) {
+      clearTimeout(takePictureTimeout.current);
+    }
+  }, [takenPicture]);
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          Camera Permission Not Granted
+        </Text>
+        <Button mode="contained" onPress={requestPermission}>
+          Grant Permission
         </Button>
       </View>
-    </SafeAreaView>
+    );
+  }
+
+  if (!takenPicture) {
+    return (
+      <View style={localStyles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>
+            Record 10 seconds video of the Exercise to analyze
+          </Text>
+        </View>
+        <View
+          style={{
+            ...localStyles.cameraContainer,
+            width: cameraWidth,
+            height: cameraHeight,
+          }}
+        >
+          <Camera
+            useCamera2Api
+            autoFocus
+            ref={cameraRef}
+            style={localStyles.camera}
+            type={type}
+          >
+            <View style={localStyles.buttonContainer}>
+              {isRecording && (<View style={{ ...localStyles.recordIndicatorContainer, position: 'absolute', left: 30 }}>
+                <View style={localStyles.recordDot} />
+                <Text style={localStyles.recordTitle}>{"Recording..."}</Text>
+              </View>)}
+              <IconButton
+                size={26}
+                icon="camera-flip-outline"
+                iconColor={MD3Colors.secondary100}
+                onPress={toggleCameraType}
+              />
+            </View>
+          </Camera>
+        </View>
+        {showCountDown ? (
+          <Text style={{ ...localStyles.countdownTimer, color: isRecording ? theme.colors.error : theme.colors.onPrimaryContainer }}>
+            {countdown === 0 ? recordingCountdown : countdown}
+          </Text>
+        ) : (
+          <View style={{ ...styles.container }}>
+            <IconButton
+              size={60}
+              iconColor="red"
+              style={{ marginTop: 16 }}
+              icon="record-rec"
+              onPress={takePicture}
+            />
+            <Button
+              icon="arrow-left"
+              mode="contained"
+              onPress={() => navigation.goBack()}
+              style={{ marginTop: 20, marginBottom: 20, marginRight: 10 }}
+            >
+              Go back
+            </Button>
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  return (
+    <View style={localStyles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>
+          Record the video of the Exercise to analyze
+        </Text>
+      </View>
+      <View
+        style={{
+          ...localStyles.cameraContainer,
+          width: cameraWidth,
+          height: cameraHeight,
+        }}
+      >
+        <Video
+          source={takenPicture}
+          shouldPlay
+          style={localStyles.camera}
+          isLooping
+        />
+      </View>
+      {uploadStatus ? (
+        <View style={{ marginTop: 10 }}>
+          <Text style={{ fontSize: 18 }}>
+            Uploading the video. Status: {uploadStatus}%
+          </Text>
+        </View>
+      ) : (
+        <View style={localStyles.buttonsRow}>
+          <Button
+            icon="cancel"
+            mode="contained-tonal"
+            buttonColor="red"
+            style={{ marginRight: 16 }}
+            onPress={() => setTakenPicture(null)}
+          >
+            Retake
+          </Button>
+          <Button
+            icon="check"
+            mode="contained-tonal"
+            buttonColor="green"
+            style={{ marginLeft: 16 }}
+            onPress={uploadVideoToFirebase}
+          >
+            Accept
+          </Button>
+        </View>
+      )}
+    </View>
   );
 };
+
+const localStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 56,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    width: "100%",
+  },
+  cameraContainer: {
+    width: "100%",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    overflow: "hidden",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignSelf: "flex-start",
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+    justifyContent: "flex-end",
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  sizeText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "white",
+  },
+  galleryContainer: {
+    flexDirection: "row",
+  },
+
+  galleryImage: {
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    width: 50,
+    height: 50 * (4 / 3),
+    margin: 10,
+  },
+  bodyImage: {
+    flex: 1,
+    height: "60%",
+    width: "auto",
+  },
+  buttonsRow: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  countdownTimer: {
+    textAlign: "center",
+    fontSize: 40,
+    fontWeight: "bold",
+    marginTop: 30,
+  },
+  recordIndicatorContainer: {
+    flexDirection: "row",
+    position: "absolute",
+    top: 25,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    opacity: 0.7,
+  },
+  recordTitle: {
+    fontSize: 14,
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  recordDot: {
+    borderRadius: 3,
+    height: 6,
+    width: 6,
+    backgroundColor: "#ff0000",
+    marginHorizontal: 5,
+  },
+});
