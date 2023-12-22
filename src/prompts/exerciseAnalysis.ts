49d277de006186566219bc9082f629f06951eed1
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-12-22 12:35:15 +0100

    chore: update prompt

--- src/prompts/exerciseAnalysis.ts
+++ src/prompts/exerciseAnalysis.ts
@@ -1,12 +1,12 @@
 export const exerciseAnalysisPrompt = `
 Based on the attached video, first of all, check if it's a valid exercise or not.
-If it's not a valid exercise, please describe why.
+If it's not a valid exercise, please tell me why.
 Ignore the video if it's not an exercise.
 And provide a reason why it's not an exercise, and why the video is not valid.
 Otherwise, please analyze what kind of exercise it is and what muscle groups it affects.
 Secondly, please analyze the correctness of the exercise.
 If the exercise is performed incorrectly, please describe what is wrong with it.
-Describe what the athlete is doing wrong, and what should do instead.
+Describe what am I doing wrong, and what should I do instead.
 If the exercise is performed correctly, please describe what is good about it.
 Let me know if the exercise is suitable for beginners or advanced athletes.
 Try to be as detailed as possible.
