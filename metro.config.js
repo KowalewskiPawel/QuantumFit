82cbaac7ca5698a071cc8e32f1f66c28c2352911
KowalewskiPawel <pawelkowalewsk@gmail.com>
2024-01-06 14:12:12 +0100

    chore: add deps and config metro

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push('cjs');
+defaultConfig.resolver.assetExts.push('bin');
 defaultConfig.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
 
 module.exports = defaultConfig;
