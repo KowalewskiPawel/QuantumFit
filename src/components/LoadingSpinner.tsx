af0667577c30b2f4b427aa2ce358523a25edbb7b
KrzysiekKuczma <kuczma.krzysiek@gmail.com>
2024-01-07 16:08:44 +0100

    wip

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { useTheme } from "react-native-paper";
 
-export const LoadingSpinner = ({size = 40, style  }) => {
+interface Props {
+  size?: number;
+  style?: any;
+}
+export const LoadingSpinner: React.FC<Props> = ({size = 40, style  }) => {
   const theme = useTheme()
   return (<View style={{...styles.container, ...style}}>
     <ActivityIndicator size={size} color={theme.colors.tertiary} />
