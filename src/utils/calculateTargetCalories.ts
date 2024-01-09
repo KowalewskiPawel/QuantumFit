67c4492c3c528295b101799166cc97302e65459a
KowalewskiPawel <pawelkowalewsk@gmail.com>
2024-01-09 21:33:34 +0100

    feat: add calculate target calories

---
+++ src/utils/calculateTargetCalories.ts
@@ -0,0 +1,22 @@
+export const calculateTargetCalories = (
+    weight: number,
+    height: number,
+    age: number,
+    sex: string,
+    goal: string
+  ) => {
+    let maintenanceCalories: number;
+    if (sex === "male") {
+      maintenanceCalories = 10 * weight + 6.25 * height - 5 * age + 5;
+    } else {
+      maintenanceCalories = 10 * weight + 6.25 * height - 5 * age - 161;
+    }
+  
+    if (goal.includes("lose")) {
+      maintenanceCalories = maintenanceCalories + 500;
+    } else if (goal.includes("gain")) {
+      maintenanceCalories = maintenanceCalories - 500;
+    }
+  
+    return Math.round(maintenanceCalories);
+  };
